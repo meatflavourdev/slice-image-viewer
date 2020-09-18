f801211272d1ad303d965931a602b112e1663b72
Jeremy Felix D <me@jeremyfelix.com>
2020-09-18 02:03:19 -0700

    Cleans up junk comments

--- Directory.js
+++ Directory.js
@@ -4,27 +4,33 @@ const input = require("./input");
 
 class Directory {
   constructor(inputPath) {
-    if(!inputPath) throw new Error('Directory path is empty');
+    if (!inputPath) throw new Error("Directory path is empty");
+
     let existsType = input.exists(inputPath);
-    // Get the directory path
-    this.inputDir = (existsType === 'dir') ? inputPath : path.dirname(inputPath);
-    // Get the filename if path resolves to a file
-    if(existsType === 'file') this.inputFile = path.basename(inputPath);
-    // List directory contents (supported types only) to files array
-    if(!input.isDirectory(this.inputDir)) throw new Error('Directory not found or not resolved');
-    this.files = jetpack.list(this.inputDir)
-          .filter(input.isDisplayableImage)
-          .map((fileName) => path.join(this.inputDir, fileName));
+    this.inputDir = existsType === "dir" ? inputPath : path.dirname(inputPath);
+    if (existsType === "file") this.inputFile = path.basename(inputPath);
+
+    if (!input.isDirectory(this.inputDir))
+      throw new Error("Directory not found or not resolved");
+
+    this.fileArray = jetpack
+      .list(this.inputDir)
+      .filter(input.isDisplayableImage);
+    //.map((fileName) => path.join(this.inputDir, fileName));
+
+    this.files = new Map();
+
     // Calculate index
-    if(this.files.length && this.inputFile){
-      this.index = this.files.indexOf(path.join(this.inputDir, this.inputFile));
-    } else if(this.files.length) {
+    if (this.fileArray.length && this.inputFile) {
+      //this.index = this.fileArray.indexOf(path.join(this.inputDir, this.inputFile));
+      this.index = this.fileArray.indexOf(this.inputFile);
+    } else if (this.fileArray.length) {
       this.index = 0;
-      this.inputFile = path.basename(this.files[0]);
+      this.inputFile = path.basename(this.fileArray[0]);
     } else {
-      throw new Error('No supported files found in directory');
+      throw new Error("No supported files found in directory");
     }
-    // Return Directory object
+
     return this;
   }
 }
--- FileContext.js
+++ FileContext.js
@@ -5,19 +5,18 @@ const Directory = require("./Directory");
 
 class FileContext {
   constructor(inputPath) {
-    if(!inputPath) throw new Error('Input is empty');
+    if (!inputPath) throw new Error("Input is empty");
+
     this.inputPath = input.abslolutePath(inputPath);
     this.existsType = input.exists(this.inputPath);
     this.isDisplayableImage = input.isDisplayableImage(this.inputPath);
-    // Check that the inputPath exists
-    if(!this.existsType) throw new Error('Input path does not exist');
-    // Check that the inputPath is a supported file or directory
-    if(this.existsType !== 'dir' && !this.isDisplayableImage) throw new Error('Unsupported file type');
-    // Build the directory object
+
+    if (!this.existsType) throw new Error("Input path does not exist");
+    if (this.existsType !== "dir" && !this.isDisplayableImage)
+      throw new Error("Unsupported file type");
+
     this.directory = new Directory(this.inputPath);
-    // Check that directory contains supported files
-    if(this.directory.size <= 0) throw new Error('Directory contains no supported files');
-    // Return the file context object
+
     return this;
   }
 }
