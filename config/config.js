3a46ff308330623ef058d2927b3f93b475c5bc29
Jeremy Felix D <me@jeremyfelix.com>
2020-09-18 01:59:25 -0700

    Adds jfif to supported image types in config

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 //Define config variables
 module.exports = {
-  IMAGE_EXTENSIONS: ["jpg", "jpeg", "png", "gif", "webp", "bmp", "svg"],
+  IMAGE_EXTENSIONS: ["jpg", "jpeg", "jfif", "png", "gif", "webp", "bmp", "svg"],
   //START_MAXIMIZED: true,
   //BACKGROUND_TRANSPARENT: true,
   //FULLSCREEN_AUTO_HIDE_CONTROLS: true,
