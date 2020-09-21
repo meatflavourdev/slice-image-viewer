da1d541bd37ce7f2c6576704c1386f0a32d80d6b
Jeremy Felix D <me@jeremyfelix.com>
2020-09-20 22:24:42 -0700

    Adds caller specificity to Error messages

--- FileContext.js
+++ FileContext.js
@@ -5,15 +5,15 @@ const Directory = require("./Directory");
 
 class FileContext {
   constructor(inputPath) {
-    if (!inputPath) throw new Error("Input is empty");
+    if (!inputPath) throw new Error("FileContext: Input path is empty");
 
     this.inputPath = input.abslolutePath(inputPath);
     this.existsType = input.exists(this.inputPath);
     this.isDisplayableImage = input.isDisplayableImage(this.inputPath);
 
-    if (!this.existsType) throw new Error("Input path does not exist");
+    if (!this.existsType) throw new Error("FileContext: Input path does not exist");
     if (this.existsType !== "dir" && !this.isDisplayableImage)
-      throw new Error("Unsupported file type");
+      throw new Error("FileContext: Unsupported file type");
 
     this.directory = new Directory(this.inputPath);
 
