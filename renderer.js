acfdd38a7f0a0789e49bde40a64599f07496bfcd
Jeremy Felix D <me@jeremyfelix.com>
2020-09-22 22:04:16 -0700

    Adds file-size event
    file-size event sets the --image-width and --image-height property and, for now at least, is to be fired initally on file open (not sure how property updating will work once file browsing is implemented)

--- renderer.js
+++ renderer.js
@@ -1,8 +1,18 @@
 const { ipcRenderer } = require("electron");
 
+let imageView = document.getElementById('imageView');
 let imageTag = document.getElementById('imageTag');
 
 ipcRenderer.on("file-context-data", (e, data) => {
   console.log(data.directory.inputDir + "\\" + data.directory.fileArray[data.directory.index]);
   imageTag.setAttribute('src', data.directory.inputDir + "\\" + data.directory.fileArray[data.directory.index]);
+});
+
+ipcRenderer.on('file-size', (e, data) => {
+  imageView.style.setProperty('--image-width', data.width + 'px');
+  imageView.style.setProperty('--image-height', data.height + 'px');
+  setTimeout(() => {
+    imageView.classList.add('transition');
+  }, 50)
+  
 });
