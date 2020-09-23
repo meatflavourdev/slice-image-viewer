a45d9445f312eb68e4165a4ce181ab6e88a0e49b
Jeremy Felix D <me@jeremyfelix.com>
2020-09-22 21:57:38 -0700

    Adds application title constant to config file

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 //@todo Make safe config accessor method
 module.exports = {
   IMAGE_EXTENSIONS: ["jpg", "jpeg", "jfif", "png", "gif", "webp", "bmp", "svg"],
+  APPLICATION_TITLE: "Slice Image Viewer"
   //START_MAXIMIZED: true,
   //BACKGROUND_TRANSPARENT: true,
   //FULLSCREEN_AUTO_HIDE_CONTROLS: true,
